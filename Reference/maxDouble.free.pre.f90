1d1282dcbd302255ea9a60e6244075648b79fe9e
m_utkej <m_utkej@anlextwls096-216>
2009-03-18 11:14:50 -0500

    fixed constant precision declaration (test code with mixed precision arguments to max is in fact invalid) and made it refer to the defs in the Extras file

--- Reference/maxDouble.free.pre.f90
+++ Reference/maxDouble.free.pre.f90
@@ -1,9 +1,11 @@
 program maxDouble
   implicit none
   double precision x,y
+  double precision :: oad_ctmp0
 
-  x = 3.1415D0
-  y = max(x,8.90D0)
+  x = 3.1415
+  call oad_s_max_d(x,8.90,oad_ctmp0)
+  y = oad_ctmp0
   write(*,*) 'y = ',y
 end program maxDouble
 
