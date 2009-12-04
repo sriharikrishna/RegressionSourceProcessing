5d143ab49fbcdf34c5c2eb5ea8e0a050bb782ad4
m_utkej <m_utkej@utke2.site>
2009-12-04 17:28:44 -0600

    still not hoisted but at least reflect the changes since the files are already there

--- Reference/hoistFuncCall.free.pre.f90
+++ Reference/hoistFuncCall.free.pre.f90
@@ -1,4 +1,7 @@
+module OAD_intrinsics
+end module
     program functionComment
+use OAD_intrinsics
       real :: x
 
       x = 1.5D0
@@ -6,15 +9,16 @@
     end program functionComment
 
     function f(x)
+use OAD_intrinsics
       real :: x,f
 
       f = 5
     end function f
 
     subroutine oad_s_f(x,f)
+use OAD_intrinsics
       real,intent(out) :: f
       real :: x
 
       f = 5
     end subroutine oad_s_f
-
--- Reference/hoistFuncCall.free.pre.post.f90
+++ Reference/hoistFuncCall.free.pre.post.f90
@@ -1,4 +1,8 @@
+module OAD_intrinsics
+end module
     program functionComment
+use OAD_intrinsics
+    use OAD_active
       real :: x
 
       x = 1.5D0
@@ -6,15 +10,18 @@
     end program functionComment
 
     function f(x)
+use OAD_intrinsics
+    use OAD_active
       real :: x,f
 
       f = 5
     end function f
 
     subroutine oad_s_f(x,f)
+use OAD_intrinsics
+    use OAD_active
       real,intent(out) :: f
       real :: x
 
       f = 5
     end subroutine oad_s_f
-
