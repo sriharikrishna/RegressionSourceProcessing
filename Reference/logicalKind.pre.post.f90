78bba31e4a7cc5bcb8518d6c47a7fd4827398964
utke <utke@linux-l3b2.site>
2010-09-08 14:14:34 -0500

    test case exercising fix in http://mercurial.mcs.anl.gov/ad/SourceProcessing/rev/d02cdee54b5f replicated the failure observed in https://trac.mcs.anl.gov/projects/openAD/ticket/225 and shows the fix to be working

---
+++ Reference/logicalKind.pre.post.f90
@@ -0,0 +1,13 @@
+module OAD_intrinsics
+end module
+module m1
+  logical(kind=kind(.true.)) :: l1=.false.
+end module
+
+program p
+  use OAD_intrinsics
+use OAD_active
+  use m1
+  logical(kind=kind(.true.)) :: aLogical=.true.
+  print *,aLogical,l1
+end program
