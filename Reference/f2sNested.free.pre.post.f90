ddb8692f2e30586d9b58570331c1289482b707c7
utke <utke@linux-l3b2.site>
2010-09-13 10:34:56 -0500

    test case to exercise inference fix committed in http://mercurial.mcs.anl.gov/ad/SourceProcessing/rev/1c02c79cf6f6

---
+++ Reference/f2sNested.free.pre.post.f90
@@ -0,0 +1,53 @@
+module OAD_intrinsics
+end module
+module m
+use OAD_intrinsics
+use OAD_active
+implicit none
+
+type t
+  real, dimension(2) :: a
+end type
+
+contains
+
+function bar(r)
+  real :: r,bar
+  bar = 2*r
+end function
+
+function foo(aT)
+  type(t) :: aT
+  real :: foo
+  foo=bar(aT%a(2))
+end function
+
+subroutine oad_s_bar(r,bar)
+  real,intent(out) :: bar
+  real :: r
+  bar = 2*r
+end subroutine oad_s_bar
+
+subroutine oad_s_foo(aT,foo)
+  type(t) :: aT
+  real,intent(out) :: foo
+  real :: oad_ctmp0
+  call oad_s_bar(aT%a(2),oad_ctmp0)
+  foo = oad_ctmp0
+end subroutine oad_s_foo
+
+end module
+
+program p
+  use OAD_intrinsics
+use OAD_active
+  use m
+  type(t) :: aT
+  real y
+  real :: oad_ctmp0
+  aT%a(1)=3.2
+  aT%a(2)=4.1
+  call oad_s_foo(aT,oad_ctmp0)
+  y = oad_ctmp0
+  print *,y
+end program
