fb57eb0fcd591843c0ad10f58bb604ac93f20f8c
utke <utke@linux-l3b2.site>
2010-09-15 14:28:37 -0500

    test case to exercise the fix to function return types done with http://mercurial.mcs.anl.gov/ad/SourceProcessing/rev/5970f430cfe3

---
+++ Reference/functionResultType.free.pre.f90
@@ -0,0 +1,31 @@
+module OAD_intrinsics
+end module
+module m
+
+contains
+
+function foo(x) result(y)
+use OAD_intrinsics
+  real :: x,y
+  y = 2*x
+end function
+
+subroutine oad_s_foo(x,y)
+use OAD_intrinsics
+  real,intent(out) :: y
+  real :: x
+  y = 2*x
+end subroutine oad_s_foo
+
+end module
+
+program p
+  use OAD_intrinsics
+  use m
+  implicit none
+  real :: x,y
+  real :: oad_ctmp0
+  x=2.0
+  call oad_s_foo(x,oad_ctmp0)
+  y = oad_ctmp0
+end program
