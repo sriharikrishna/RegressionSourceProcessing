475c498c958a4bb6645196566eeed712c5fde38d
utke <utke@linux-l3b2.site>
2010-09-14 23:29:11 -0500

    simple functions nested in subroutines

---
+++ Reference/nestedProc.free.pre.f90
@@ -0,0 +1,39 @@
+module OAD_intrinsics
+end module
+subroutine s1()
+  real :: x
+  contains
+  real function foo(x)
+use OAD_intrinsics
+    integer:: x
+    foo=x
+  end function
+  subroutine oad_s_foo(x,foo)
+use OAD_intrinsics
+    integer:: x
+    real,intent(out) :: foo
+    foo=x
+  end subroutine oad_s_foo
+end subroutine
+
+subroutine s2()
+  real :: x
+  contains
+  real function foo(x)
+use OAD_intrinsics
+   integer :: x
+   foo=x
+  end function
+  subroutine oad_s_foo(x,foo)
+use OAD_intrinsics
+   integer :: x
+   real,intent(out) :: foo
+   foo=x
+  end subroutine oad_s_foo
+end subroutine
+
+program p
+use OAD_intrinsics
+  call s1
+  call s2
+end program
---
+++ Reference/nestedProc.free.pre.post.f90
@@ -0,0 +1,44 @@
+module OAD_intrinsics
+end module
+subroutine s1()
+  real :: x
+  contains
+  real function foo(x)
+use OAD_intrinsics
+  use OAD_active
+    integer:: x
+    foo=x
+  end function
+  subroutine oad_s_foo(x,foo)
+use OAD_intrinsics
+  use OAD_active
+    integer:: x
+    real,intent(out) :: foo
+    foo=x
+  end subroutine oad_s_foo
+end subroutine
+
+subroutine s2()
+  real :: x
+  contains
+  real function foo(x)
+use OAD_intrinsics
+  use OAD_active
+   integer :: x
+   foo=x
+  end function
+  subroutine oad_s_foo(x,foo)
+use OAD_intrinsics
+  use OAD_active
+   integer :: x
+   real,intent(out) :: foo
+   foo=x
+  end subroutine oad_s_foo
+end subroutine
+
+program p
+use OAD_intrinsics
+use OAD_active
+  call s1()
+  call s2()
+end program
