cd098f90a5e900988ba04b75a66cccdc2fc8c9ec
utke <utke@linux-l3b2.site>
2010-10-04 16:46:52 -0500

    test cases exercising change done with http://mercurial.mcs.anl.gov/ad/SourceProcessing/rev/39641cc2df92

---
+++ PrePost/Reference/drvdTypeArrayShapeInference.free.pre.post.f90
@@ -0,0 +1,31 @@
+module OAD_intrinsics
+end module
+function foo(i)
+use OAD_intrinsics
+use OAD_active
+  real :: i,foo
+  foo = 2*i
+end function
+subroutine oad_s_foo(i,foo)
+use OAD_intrinsics
+use OAD_active
+  real,intent(out) :: foo
+  real :: i
+  foo = 2*i
+end subroutine oad_s_foo
+
+program p
+use OAD_intrinsics
+use OAD_active
+  type t
+   real, dimension(2) :: i
+   integer :: idx=2
+  end type
+  real y, foo
+  type(t) :: aT
+  real :: oad_ctmp0
+  aT%i(1)=2.0
+  call oad_s_foo(aT%i(aT%idx),oad_ctmp0)
+  y = oad_ctmp0
+  print *,y
+end program
