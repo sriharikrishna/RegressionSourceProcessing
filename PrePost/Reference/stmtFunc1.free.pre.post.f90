f36efa7eeed5a422c18eaa70e1a06d92d5f1a453
utke <utke@linux-l3b2.site>
2010-10-15 09:19:32 -0500

    statement function test exercising fixes up to (http://mercurial.mcs.anl.gov/ad/SourceProcessing/rev/6267347cd8dc ), see also http://trac.mcs.anl.gov/projects/openAD/ticket/244

---
+++ PrePost/Reference/stmtFunc1.free.pre.post.f90
@@ -0,0 +1,20 @@
+module OAD_intrinsics
+end module
+subroutine foo(i)
+use OAD_intrinsics
+use OAD_active
+print *,i
+end
+
+program p
+use OAD_intrinsics
+use OAD_active
+implicit integer (f)
+integer i
+fun(l) = i+l
+  integer :: oad_ctmp0
+i=3
+oad_ctmp0 = i+2
+call foo(oad_ctmp0)
+end
+
