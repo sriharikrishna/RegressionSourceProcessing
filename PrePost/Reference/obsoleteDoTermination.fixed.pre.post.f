d96209dce97e44333152011a25fd7d89cfd21001
utke <utke@linux-l3b2.site>
2010-10-20 15:25:21 -0500

    exercising fix  http://mercurial.mcs.anl.gov/ad/SourceProcessing/rev/5a573e8582f1

---
+++ PrePost/Reference/obsoleteDoTermination.fixed.pre.post.f
@@ -0,0 +1,16 @@
+module OAD_intrinsics
+end module
+ program p
+  use OAD_intrinsics
+ use OAD_active
+  integer i
+  integer j
+  j=1
+  do 4 i=1,3
+     j = j+1
+  if (j>2) then
+    write(*,*) j
+  end if
+ 4      end do
+ end program
+
