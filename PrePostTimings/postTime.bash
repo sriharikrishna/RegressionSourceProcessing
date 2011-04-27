96464d0eb1427fcebe67fdf813a00118e65018e9
utke on macbook <utke on macbook@localhost>
2011-04-27 10:01:33 -0500

    echo the script name so I can grep in the cron output

--- PrePostTimings/postTime.bash
+++ PrePostTimings/postTime.bash
@@ -1,2 +1,3 @@
 #! /bin/bash
+echo -n "$0 "
 ../../postProcess.py --timing --noWarnings -m r --width 4  postInput.f 
--- PrePostTimings/preTime.bash
+++ PrePostTimings/preTime.bash
@@ -1,2 +1,4 @@
 #! /bin/bash
+echo -n "$0 "
 ../../preProcess.py --timing --noWarnings -H -S preInput.f -o preOutput.f
+
